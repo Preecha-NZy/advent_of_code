2b634918091681e627e001f2cd4808b0d74d5615
preecha <preechac@hellospec.com>
2023-12-06 22:19:04 +0700

    Refactor AoC 2023 day 6 codes.
    - using `map` instead of `each` for array
    - using `find` to find the `start_winning` instead of loop and checking
    - using `reduce` instead of `inject` copilot told me `It's more idiomatic in Ruby.`

--- 2023/6_1.rb
+++ 2023/6_1.rb
@@ -4,21 +4,9 @@ Distance:  9  40  200".split("\n")
 times = inputs[0].split(":")[1].split.map(&:to_i)
 distances = inputs[1].split(":")[1].split.map(&:to_i)
 
-winning_probability = []
-start_winning = 0
-times.each.with_index do |t, i|
-  winning_time = 0
-  t.times do |j|
-    if (t - j) * j > distances[i]
-      start_winning = j
-      break
-    end
-  end
-
+winning_probability = times.map.with_index do |t, i|
+  start_winning = (0...t).find { |j| (t - j) * j > distances[i] } || 0
   winning_time = (t + 1) - start_winning * 2
-  winning_probability << winning_time
-
-  puts "start_winning: #{start_winning}, t: #{t}, winning_time: #{winning_time}"
 end
 
-p winning_probability.inject(:*)
+puts winning_probability.reduce(:*)
--- 2023/6_2.rb
+++ 2023/6_2.rb
@@ -4,18 +4,10 @@ Distance:  9  40  200".split("\n")
 times = inputs[0].split(":")[1].split.map(&:to_i)
 distances = inputs[1].split(":")[1].split.map(&:to_i)
 
-time = times.inject{ |sum, n| sum.to_s + n.to_s }.to_i
-distance = distances.inject{ |sum, n| sum.to_s + n.to_s }.to_i
+time = times.join.to_i
+distance = distances.join.to_i
 
-puts "#{time} #{distance}"
-
-start_winning = 0
-time.times do |t|
-  if (time - t) * t > distance
-    start_winning = t
-    break
-  end
-end
-
+start_winning = (0...time).find { |t| (time - t) * t > distance } || 0
 winning_probability = (time + 1) - start_winning * 2
+
 puts winning_probability
